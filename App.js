78d02b2904c6488c000cd1aa0183edd468fda401
Artem Kraievyi <artem.kraievyi@gmail.com>
2024-01-14 22:08:56 +0200

    feat: receiving from API and output to the user

--- App.js
+++ App.js
@@ -1,8 +1,24 @@
+import axios from 'axios'
+import React from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { Text, View, Image } from 'react-native'
 import { Post } from './components/Post'
 
 export default function App() {
+  const [items, setItems] = React.useState()
+
+  React.useEffect(() => {
+    axios
+      .get('https://65a43ae652f07a8b4a3d4245.mockapi.io/posts/v1/articles')
+      .then(({ data }) => {
+        setItems(data)
+      })
+      .catch((err) => {
+        console.log(err)
+        alert('Ошибка при получении статей')
+      })
+  }, [])
+
   return (
     <View>
       <StatusBar theme="auto" />
