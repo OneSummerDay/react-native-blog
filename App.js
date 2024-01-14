2936679b0db83e7a7e643003d4259075b3dfc570
Artem Kraievyi <artem.kraievyi@gmail.com>
2024-01-14 21:18:56 +0200

    style: add Post and PostImage layout

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { Text, View, Image } from 'react-native'
+import styled from 'styled-components/native'
+
+const Post = styled.View`
+  flex: 1;
+  padding: 15px;
+  border-buttom-width: 1px;
+  border-bottom-color: rgba(0, 0, 0, 0.1);
+  border-bottom-style: solid;
+`
+
+const PostImage = styled.Image`
+  width: 60px;
+  height: 60px;
+  border-radius: 12px;
+  margin-right: 12px;
+`
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <StatusBar theme="auto" />
+      <Post>
+        <Image
+          source={{ uri: 'https://images.app.goo.gl/SpQ2bWgBHdgLuEF18' }}
+        />
+      </Post>
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
