1a3233f412024e2b9253d54660c57f66a0928dad
Artem Kraievyi <artem.kraievyi@gmail.com>
2024-01-15 13:41:47 +0200

    refactor: loading app logik

--- App.js
+++ App.js
@@ -26,22 +26,23 @@ export default function App() {
 
   React.useEffect(fetchPosts, [])
 
-  is (isLoading) {
+  if (isLoading) {
     return (
-    <View style={{
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    }}>
-      <ActivityIndicator size="large" />
-      <Text>Загрузка...</Text>
-    </View>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <ActivityIndicator size="large" />
+        <Text>Загрузка...</Text>
+      </View>
     )
   }
 
   return (
     <View>
-      
       <FlatList
         data={items}
         renderItem={({ item }) => (
